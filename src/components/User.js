36ad00751d57286598e6b4dfe7f020f0442a34b4
Rik <ingeniero_rik@hotmail.com>
2023-04-08 11:36:20 -0300

    probando conexion con apis

--- src/components/User.js
+++ src/components/User.js
@@ -15,7 +15,7 @@ class User extends Component{
     //Compomentes Ciclo de vida - Montar - Actualizar - Desmontar
     //Montaje
     componentDidMount(){
-        fetch('https://stoneblack.onrender.com/api/usuarios')  //<------------------------------       /movies
+        fetch('/api/usuarios')  //<------------------------------       /movies
         .then(respuesta =>{
             return respuesta.json()
         })
