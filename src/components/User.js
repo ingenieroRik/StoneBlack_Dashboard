5ce52388c48044290e797a69c7512cc72d3c6fd5
Rik <ingeniero_rik@hotmail.com>
2023-04-11 00:37:56 -0300

    falta ,

--- src/components/User.js
+++ src/components/User.js
@@ -16,7 +16,7 @@ function User() {
     const [recuperado, setRecuperado] = useState(false)
 
     useEffect(() => {
-            fetch("https://stoneblack.onrender.com/api/usuarios"{
+            fetch("https://stoneblack.onrender.com/api/usuarios",{
                 mode: 'no-cors',
                 credentials: 'omit'
             }
